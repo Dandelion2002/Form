be1a8e41cd2a3f4ff29256d147ec1275a7413f63
Dandelion2002 <kurapika.ml@gmail.com>
2022-02-09 14:50:20 +0100

    commit

---
+++ validation.js
@@ -0,0 +1,22 @@
+//Todos los campos obligatorios--> aviso de error
+//Correo Electrónico, DNI, IBAN, SWIFT bien formados
+//Num tel--> al menos nueve números con posibilidad de espacios
+//Todo correcto mostrar un alert con todo correcto, si no error
+
+function todoCorrecto(){
+    var correcto = true;
+    console.log("ok");
+    
+}
+/*
+    console.log(formularioPrincipal.getElementsByTagName("name"));
+    console.log(formularioPrincipal.getElementsByTagName("surname"));
+    console.log(formularioPrincipal.getElementsByTagName("mail"));
+    console.log(formularioPrincipal.getElementById("phone"));
+    console.log(formularioPrincipal.getElementById("dni"));
+    console.log(formularioPrincipal.getElementById("iban"));
+    console.log(formularioPrincipal.getElementById("swift"));
+    console.log(formularioPrincipal.getElementById("username"));
+    console.log(formularioPrincipal.getElementById("birth"));
+*/
+
